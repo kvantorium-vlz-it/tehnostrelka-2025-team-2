0ef15e7f8dfb66a657534ca28fd1e207ab23b80a
Akzis <akzi31xx@gmail.com~>
2025-02-09 22:36:29 +0300

    (исправлние)Создание маршрута,фото,точек

--- route.py
+++ route.py
@@ -14,18 +14,9 @@ class Route(SQLModel, table=True):
     description: str
     creator: str
     created_at: datetime = Field(default_factory=datetime.now, nullable=False)
-    created_at: str
     is_private: bool
     points: List["RoutePoint"] = Relationship(back_populates="route")
-
-
-class RouteImage(SQLModel, table=True):
-    id: Optional[int] = Field(default=None, primary_key=True)
-    photo_url: Optional[str] = None
-    photo_id: Optional[int] = Field(default=None, foreign_key="photo.id")
-
-
-
+    photos: List["RoutePhoto"] = Relationship(back_populates="route")
 
 class RoutePoint(SQLModel, table=True):
     id: Optional[int] = Field(default=None, primary_key=True)
@@ -33,6 +24,12 @@ class RoutePoint(SQLModel, table=True):
     longitude: float
     route_id: Optional[int] = Field(default=None, foreign_key="route.id")
     route: Optional[Route] = Relationship(back_populates="points")
+
+class RoutePhoto(SQLModel, table=True):
+    id: Optional[int] = Field(default=None, primary_key=True)
+    url: Optional[str] = None
+    route_id: Optional[int] = Field(default=None, foreign_key="route.id")
+    route: Optional[Route] = Relationship(back_populates="photos")
 
 
 
@@ -57,6 +54,40 @@ def read_routes():
         routes = session.exec(select(Route)).all()
         return routes
 
+#создание маршрута
+@app.post("/create_route/", response_model=Route)
+def create_route(route: Route):
+    with Session(engine) as session:
+        session.add(route)
+        session.commit()
+        session.refresh(route)
+    return route
+
+#создание фото к маршруту
+@app.post("/routes/{route_id}/images/", response_model=RoutePhoto)
+def create_route_image(route_id: int, route_photo: RoutePhoto):
+    route_photo.route_id = route_id
+    with Session(engine) as session:
+        route = session.get(Route, route_id)
+        if not route:
+            raise HTTPException(status_code=404, detail="Route not found")
+        session.add(route_photo)
+        session.commit()
+        session.refresh(route_photo)
+    return route_photo
+
+#создание точек к маршруту
+@app.post("/routes/{route_id}/points/", response_model=RoutePoint)
+def create_route_point(route_id: int, route_point: RoutePoint):
+    route_point.route_id = route_id
+    with Session(engine) as session:
+        route = session.get(Route, route_id)
+        if not route:
+            raise HTTPException(status_code=404, detail="Route not found")
+        session.add(route_point)
+        session.commit()
+        session.refresh(route_point)
+    return route_point
 
 
 # if (__name__ == "__main__"):
