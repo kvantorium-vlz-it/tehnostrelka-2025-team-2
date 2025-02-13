5fb54c0e1052e5ffeaf05487edf520770332173a
Zandyss <nikitabarladyan@gmail.com>
2025-02-13 19:34:06 +0300

    Удалил css

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -3,7 +3,6 @@ import vuetify, { transformAssetUrls } from 'vite-plugin-vuetify'
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
-  css: ['@/assets/main.css'],
   build: {
     transpile: ['vuetify'],
   },
